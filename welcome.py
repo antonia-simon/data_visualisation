758b97725e908b31760341478ce56c1983b9330e
antonia-simon <141242088+antonia-simon@users.noreply.github.com>
2023-08-03 10:24:14 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import os
+from bokeh.plotting import figure
 
 file_name_list = []                   # initialisation: give a value to variable
 for i in os.listdir():                # select each i in the directory (each .csv file)
@@ -16,3 +17,16 @@ el_list = df.columns.tolist()[27:80]
 x_axis = st.selectbox('select element', el_list)
 
 st.multiselect('select location', file_name_list, file_name_list[0])    # to select more than one element
+
+
+x = [1, 2, 3, 4, 5]
+y = [6, 7, 2, 4, 5]
+
+p = figure(
+    title='simple line example',
+    x_axis_label='x',
+    y_axis_label='y')
+
+p.line(x, y, legend_label='Trend', line_width=2)
+
+st.bokeh_chart(p, use_container_width=True)
