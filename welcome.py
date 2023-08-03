97f128a9122bdc8b9f7e086f057cffbe086c7073
antonia-simon <141242088+antonia-simon@users.noreply.github.com>
2023-08-03 09:54:28 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -15,4 +15,4 @@ st.dataframe(df)
 el_list = df.columns.tolist()[27:80]
 x_axis = st.selectbox('select element', el_list)
 
-st.multiselect('select location', file_name_list)    # to select more than one element
+st.multiselect('select location', file_name_list, file_name_list[0])    # to select more than one element
