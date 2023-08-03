0c5481b522c54e5f45e60efc83c1277533ccc6a2
antonia-simon <141242088+antonia-simon@users.noreply.github.com>
2023-08-03 12:36:57 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -17,14 +17,14 @@ el_list = df.columns.tolist()[27:80]
 x_axis = st.selectbox('select x element', el_list)
 y_axis = st.selectbox('select y element', el_list)
 
-st.multiselect('select location', file_name_list)     # to select more than one element
-                                                                         # reboot app under manage app
+select_data = st.multiselect('select location', file_name_list)                         # to select more than one element
+                                                                                         # reboot app under manage app
 colorlist = ['red', 'green', 'yellow', 'blue']
 
 p = figure(x_axis_label=x_axis + ' [wt.%]', y_axis_label=y_axis + ' [wt.%]')
 
-for i in range(len(file_name_list)):
-  df2 = pd.read_csv(file_name_list[i])
+for i in range(len(select_data)):
+  df2 = pd.read_csv(select_data[i])
   p.circle(df2[x_axis]/10000, df2[y_axis]/10000, color = colorlist[i])
 
 
